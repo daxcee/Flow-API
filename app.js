42ef2b7962cab371bc3d859153179d1ee0a92f07
Steven <stevenramdas@gmail.com>
2015-05-17 20:53:46 +0200

    Move mongodb driver to artists endpoint, serve json response

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -7,13 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var app = express();
-var host = process.env.DB_HOST;
-var db_port = process.env.DB_PORT;
-var db_name = process.env.DB_NAME;
-var db_user = process.env.DB_USER;
-var db_pass = process.env.DB_PASSWORD;
-var url = 'mongodb://' + db_user + ':' + db_pass + '@' + host + ':' + db_port + '/' + db_name;
-var MongoClient = require('mongodb').MongoClient;
 //app.set('port', (process.env.PORT || 3000)); //enable this when deploying to Heroku.
 
 app.set('views', path.join(__dirname, 'views'));
@@ -53,17 +45,6 @@ app.use(function(err, req, res, next) {
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
-});
-
-MongoClient.connect(url, function(err, db) {
-    if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-        console.log("Connected correctly to: ", url);
-    }
-
-    //Close connection
-    db.close();
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,52 @@
+require('dotenv').load();
 var express = require('express');
 var router = express.Router();
 var app = express();
 var port = process.argv[2] || 3000;
 var http = require('http');
 app.set('json spaces', 2);
+var host = process.env.DB_HOST;
+var db_port = process.env.DB_PORT;
+var db_name = process.env.DB_NAME;
+var db_user = process.env.DB_USER;
+var db_pass = process.env.DB_PASSWORD;
+var url = 'mongodb://' + db_user + ':' + db_pass + '@' + host + ':' + db_port + '/' + db_name;
+var MongoClient = require('mongodb').MongoClient;
 
 router.get('/', function(request, response) {
   response.send('Flow API is running.');
 });
 
 router.get('/api/artists', function(req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify("Foo:bar"));
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+            console.log("Connected correctly to: ", url);
+
+            var collection = db.collection('artists');
+
+            // Query DB, artist collection
+            collection.find({mediaType: 'bio'}).toArray(function (err, result) {
+                if (err) {
+                    console.log(err);
+                    res.setHeader("Content-Type", "application/json");
+                    res.end(err);
+                } else if (result.length) {
+                    console.log('Found:', result);
+                    res.setHeader("Content-Type", "application/json");
+                    res.end(JSON.stringify(result));
+                } else {
+                    console.log('No document(s) found with defined "find" criteria!');
+                    res.setHeader("Content-Type", "application/json");
+                    res.end('No document(s) found with defined "find" criteria!');
+                }
+                //Close connection
+                db.close();
+            });
+        }
+    });
+
 });
 
 router.get('/api/tracks', function(req, res) {
