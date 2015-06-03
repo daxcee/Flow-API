e98c73e11289e0a4f42df1adb9befff9fdc41003
srmds <stevenramdas@gmail.com>
2015-06-03 23:12:37 +0200

    Delegate genre endpoint request to controller

---
+++ controllers/genreController.js
@@ -0,0 +1,55 @@
+require('../models/genre')();
+
+var mongoose = require('mongoose');
+var config = require('config');
+
+//max 4 connections in pool
+var conn = mongoose.createConnection(config.get('db_uri'),{ server: { poolSize: 4 }});
+//Genre model is scoped to above specific connection object
+var Genre = conn.model('Genre');
+
+module.exports = {
+    getAllGenres: function (req, res) {
+        Genre.find({}, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.statusCode = 500;
+                throw err;
+            }
+
+            if (result.length) {
+                console.log('[getAllGenres]: ', result);
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify(result, null, 2));
+            } else {
+                console.log('[getAllGenres]: No genre(s) found.');
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify([], null, 2));
+            }
+        });
+    },
+
+    getGenreById: function (req, res) {
+        Genre.find({'genreName': req.params.id}, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.statusCode = 500;
+                throw err;
+            }
+
+            if (result.length) {
+                console.log('[getGenreById]: ', result);
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify(result, null, 2));
+            } else {
+                console.log('[getGenreById]: No genre(s) found for: ' + req.params.id);
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify([], null, 2));
+            }
+        });
+    }
+};
--- routes/genreEndpoint.js
+++ routes/genreEndpoint.js
@@ -1,60 +1,10 @@
-require('../models/genre')();
-
+var controllerLoader = require('../controllers/controllerLoader.js');
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var config = require('config');
 
-//max 4 connections in pool
-var conn = mongoose.createConnection(config.get('db_uri'),{ server: { poolSize: 4 }});
-//Genre model is scoped to above specific connection object
-var Genre = conn.model('Genre');
+var base = '/api/v1/genres';
 
-var base = '/api/v1';
-var path = base + '/genres';
-
-router.get(path, function(req, res) {
-    Genre.find({}, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.statusCode = 500;
-            throw err;
-        }
-
-        if (result.length) {
-            console.log('Result:', result);
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.end(JSON.stringify(result, null, 2));
-        } else {
-            console.log('No document(s) found with defined criteria!');
-            res.statusCode = 404;
-            res.setHeader("Content-Type", "text/html; charset=utf-8");
-            res.end('No document(s) found with defined criteria!');
-        }
-    });
-});
-
-router.get(path + '/:id', function(req, res) {
-    Genre.find({'genreName': req.params.id}, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.statusCode = 500;
-            throw err;
-        }
-
-        if (result.length) {
-            console.log('Result:', result);
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.end(JSON.stringify(result, null, 2));
-        } else {
-            console.log('No genre(s) found for: ' + req.params.id);
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.end(JSON.stringify([], null, 2));
-        }
-    });
-});
+router.get(base, controllerLoader.genreController.getAllGenres);
+router.get(base + '/:id', controllerLoader.genreController.getGenreById);
 
 module.exports = router;
