3e365b9e25c518f057ed5359d1569f6209dd2fb6
srmds <stevenramdas@gmail.com>
2015-06-03 23:23:29 +0200

    Delegate track endpoint request to controller

---
+++ controllers/trackController.js
@@ -0,0 +1,96 @@
+require('../models/track')();
+require('../models/artist')();
+
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var config = require('config');
+
+//max 4 connections in pool
+var conn = mongoose.createConnection(config.get('db_uri'),{ server: { poolSize: 4 }});
+//Track and Artist models are scoped to above specific connection object
+var Track = conn.model('Track');
+var Artist = conn.model('Artist');
+
+module.exports = {
+    getAllTracks: function (req, res) {
+        Track.find({}, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.statusCode = 500;
+                throw err;
+            }
+
+            if (result.length) {
+                console.log('[getAllTracks]: ', result);
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify(result, null, 2));
+            } else {
+                console.log('[getAllTracks]: No tracks(s) found for: ' + req.params.id);
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify([], null, 2));
+            }
+        });
+    },
+
+    getTrackById: function (req, res) {
+        Track.find({'_id': req.params.id}, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.statusCode = 500;
+                throw err;
+            }
+
+            if (result.length) {
+                console.log('[getTrackById]: ', result);
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify(result, null, 2));
+            } else {
+                console.log('[getTrackById]: Track: ' + req.params.id + ' not found.');
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify([], null, 2));
+            }
+        });
+    },
+
+    getTrackByArtistId: function (req, res) {
+        Artist.findOne({'artistName': req.params.id}, function (err, artist) {
+            if (err) {
+                console.log(err);
+                res.statusCode = 500;
+                throw err;
+            }
+
+            if (artist) {
+                Track.find({'artists.artistName':  artist.artistName}, function (err, result){
+                    if (err) {
+                        console.log(err);
+                        res.statusCode = 500;
+                        throw err;
+                    }
+
+                    if (result.length) {
+                        console.log('[getTrackByArtistId]: ', result);
+                        res.statusCode = 200;
+                        res.setHeader("Content-Type", "application/json");
+                        res.end(JSON.stringify(result, null, 2));
+                    } else {
+                        console.log('[getTrackByArtistId]: No tracks(s) found for: ' + req.params.id);
+                        res.statusCode = 200;
+                        res.setHeader("Content-Type", "application/json");
+                        res.end(JSON.stringify(result, null, 2));
+                    }
+                });
+            } else {
+                console.log('[getTrackByArtistId]: Artist: ' + req.params.id + ' not found.');
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify([], null, 2));
+            }
+        });
+    }
+};
--- routes/trackEndpoint.js
+++ routes/trackEndpoint.js
@@ -1,78 +1,11 @@
-require('../models/track')();
-require('../models/artist')();
-
+var controllerLoader = require('../controllers/controllerLoader.js');
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var config = require('config');
 
-//max 4 connections in pool
-var conn = mongoose.createConnection(config.get('db_uri'),{ server: { poolSize: 4 }});
-//Track model is scoped to above specific connection object
-var Track = conn.model('Track');
-var Artist = conn.model('Artist');
+var base = '/api/v1/tracks';
 
-var base = '/api/v1';
-var path = base + '/tracks';
-
-router.get(path, function(req, res) {
-    Track.find({}, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.statusCode = 500;
-            throw err;
-        }
-
-        if (result.length) {
-            console.log('Result:', result);
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.end(JSON.stringify(result, null, 2));
-        } else {
-            console.log('No tracks(s) found for: ' + req.params.id);
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.end(JSON.stringify([], null, 2));
-        }
-    });
-});
-
-//Get track(s) of an artist
-router.get(path + '/:id', function(req, res) {
-    Artist.findOne({'artistName': req.params.id}, function (err, artist) {
-        if (err) {
-            console.log(err);
-            res.statusCode = 500;
-            throw err;
-        }
-
-        if (artist) {
-            Track.find({'artists.artistName':  artist.artistName}, function (err, result){
-                if (err) {
-                    console.log(err);
-                    res.statusCode = 500;
-                    throw err;
-                }
-
-                if (result.length) {
-                    console.log('Result:', result);
-                    res.statusCode = 200;
-                    res.setHeader("Content-Type", "application/json");
-                    res.end(JSON.stringify(result, null, 2));
-                } else {
-                    console.log('No album(s) found for: ' + req.params.id);
-                    res.statusCode = 200;
-                    res.setHeader("Content-Type", "application/json");
-                    res.end(JSON.stringify(result, null, 2));
-                }
-            });
-        } else {
-            console.log('Artist: ' + req.params.id + ' not found.');
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.end(JSON.stringify([], null, 2));
-        }
-    });
-});
+router.get(base, controllerLoader.trackController.getAllTracks);
+router.get(base + '/:id', controllerLoader.trackController.getTrackById);
+router.get(base + '/artist/:id', controllerLoader.trackController.getTrackByArtistId);
 
 module.exports = router;
