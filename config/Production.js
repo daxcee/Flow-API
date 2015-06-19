491d1ec9da65244bb01e49f9e01da3dfb7988e24
srmds <stevenramdas@gmail.com>
2015-06-19 17:16:42 +0200

    update prod env settings

--- config/Production.js
+++ config/Production.js
@@ -2,5 +2,8 @@ require('dotenv').load();
 
 module.exports = {
     db_uri: 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME,
-    app_url: process.env.APP_URL
+    app_url: process.env.APP_URL,
+    ms_user:process.env.MS_USER,
+    ms_pass:process.env.MS_PASSWORD,
+    ms_host:process.env.MS_HOST
 };
