47971452fadb6e61b04665cb2bf481382693f9dc
srmds <stevenramdas@gmail.com>
2015-06-19 19:07:32 +0200

    add authentication to video paths

--- routes/videoEndpoint.js
+++ routes/videoEndpoint.js
@@ -1,11 +1,23 @@
 var controllerLoader = require('../controllers/controllerLoader.js');
 var express = require('express');
 var router = express.Router();
+var passwordless = require('passwordless');
 
 var base = '/api/v1/videos';
 
-router.get(base, controllerLoader.videoController.getAllVideos);
-router.get(base + '/:id', controllerLoader.videoController.getVideoById);
-router.get(base + '/artist/:id', controllerLoader.videoController.getVideoByArtistId);
+router.get(base, passwordless.restricted(),
+    function() {
+        router.get(base, controllerLoader.videoController.getAllVideos);
+    });
+
+router.get(base + '/:id', passwordless.restricted(),
+    function() {
+        router.get(base + '/:id', controllerLoader.videoController.getVideoById);
+    });
+
+router.get(base + '/:id/artist', passwordless.restricted(),
+    function() {
+        router.get(base + '/:id/artist', controllerLoader.videoController.getVideoByArtistId);
+    });
 
 module.exports = router;
