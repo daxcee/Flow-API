dfe5ee5d815367ca8ba8ebebb9b8696a4eb0fc5e
srmds <stevenramdas@gmail.com>
2015-06-19 19:03:58 +0200

    add authentication to event paths

--- routes/eventEndpoint.js
+++ routes/eventEndpoint.js
@@ -1,14 +1,38 @@
 var controllerLoader = require('../controllers/controllerLoader.js');
 var express = require('express');
 var router = express.Router();
+var passwordless = require('passwordless');
 
 var base = '/api/v1/events';
 
-router.get(base, controllerLoader.eventController.getAllEvents);
-router.get(base + '/:id',controllerLoader.eventController.getEventById);
-router.get(base + '/date/:id',controllerLoader.eventController.getEventByDate);
-router.get(base + '/genre/:id',controllerLoader.eventController.getEventByGenre);
-router.get(base + '/artist/:id',controllerLoader.eventController.getEventByArtist);
-router.get(base + '/city/:id',controllerLoader.eventController.getEventByCity);
+router.get(base, passwordless.restricted(),
+    function() {
+        router.get(base, controllerLoader.eventController.getAllEvents);
+    });
+
+router.get(base + '/:id', passwordless.restricted(),
+    function() {
+        router.get(base + '/:id',controllerLoader.eventController.getEventById);
+    });
+
+router.get(base + '/:id/date', passwordless.restricted(),
+    function() {
+        router.get(base + '/:id/date',controllerLoader.eventController.getEventByDate);
+    });
+
+router.get(base + '/:id/genre', passwordless.restricted(),
+    function() {
+        router.get(base + '/:id/genre',controllerLoader.eventController.getEventByGenre);
+    });
+
+router.get(base + '/:id/artist', passwordless.restricted(),
+    function() {
+        router.get(base + '/:id/artist',controllerLoader.eventController.getEventByArtist);
+    });
+
+router.get(base + '/:id/city', passwordless.restricted(),
+    function() {
+        router.get(base + '/:id/city',controllerLoader.eventController.getEventByCity);
+    });
 
 module.exports = router;
