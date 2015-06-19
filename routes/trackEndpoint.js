a94e1d42e1486988073bfab4e534054fcb8abecb
srmds <stevenramdas@gmail.com>
2015-06-19 19:06:28 +0200

    add authentication to track paths

--- routes/trackEndpoint.js
+++ routes/trackEndpoint.js
@@ -1,11 +1,23 @@
 var controllerLoader = require('../controllers/controllerLoader.js');
 var express = require('express');
 var router = express.Router();
+var passwordless = require('passwordless');
 
 var base = '/api/v1/tracks';
 
-router.get(base, controllerLoader.trackController.getAllTracks);
-router.get(base + '/:id', controllerLoader.trackController.getTrackById);
-router.get(base + '/artist/:id', controllerLoader.trackController.getTrackByArtistId);
+router.get(base, passwordless.restricted(),
+    function() {
+        router.get(base, controllerLoader.trackController.getAllTracks);
+    });
+
+router.get(base + '/:id', passwordless.restricted(),
+    function() {
+        router.get(base + '/:id', controllerLoader.trackController.getTrackById);
+    });
+
+router.get(base + '/:id/artist', passwordless.restricted(),
+    function() {
+        router.get(base + '/:id/artist', controllerLoader.trackController.getTrackByArtistId);
+    });
 
 module.exports = router;
