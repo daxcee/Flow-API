aaf7dc9c78fd120564314a9e923fa3e7632e1890
srmds <stevenramdas@gmail.com>
2015-06-19 18:59:43 +0200

    add authentication to artists paths

--- routes/artistEndpoint.js
+++ routes/artistEndpoint.js
@@ -1,10 +1,17 @@
 var controllerLoader = require('../controllers/controllerLoader.js');
 var express = require('express');
 var router = express.Router();
+var passwordless = require('passwordless');
 
 var base = '/api/v1/artists';
+router.get(base, passwordless.restricted(),
+    function() {
+        router.get(base, controllerLoader.artistController.getAllArtists);
+    });
+router.get(base + '/:id', passwordless.restricted(),
+    function() {
+        router.get(base + '/:id',controllerLoader.artistController.getArtistById);
+    });
 
-router.get(base, controllerLoader.artistController.getAllArtists);
-router.get(base + '/:id',controllerLoader.artistController.getArtistById);
 
 module.exports = router;
