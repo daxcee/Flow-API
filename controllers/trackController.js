437b2f28a600fd67bb0b66f3bcf1c1bb9beb09a7
srmds <stevenramdas@gmail.com>
2015-06-23 21:56:28 +0200

    #14 Implement pagination on tracks endpoint

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -6,6 +6,8 @@ var mongoose = require('mongoose');
 var config = require('config');
 var excep = require('../utils/exception');
 var pretty = require('../utils/pretty');
+var serverResponse = require('../utils/serverResponse.js');
+var query = require('../queriers/tracksQuerier.js');
 
 //max 4 connections in pool
 var conn = mongoose.createConnection(config.get('db_uri'),{ server: { poolSize: 4 }});
@@ -19,31 +21,18 @@ module.exports = {
         var apikey = req.param('apikey');
         Token.findOne({'value': apikey}, function (err, token) {
             if (err) {
-                res.statusCode = 500;
-                res.end(pretty.print(excep.msg(500, 'Server Error', err)));
+                serverResponse.error(err);
                 return;
             }
             if (token) {
-                Track.find({}, function (err, result) {
-                    res.setHeader("Content-Type", "application/json");
+                var params = {
+                    limit:req.param('limit'),
+                    offset:req.param('offset')
+                };
 
-                    if (err) {
-                        console.log(err);
-                        res.statusCode = 500;
-                        res.end(pretty.print(excep.msg(500, 'Server Error', err)));
-                        return;
-                    }
-                    if (result.length) {
-                        res.statusCode = 200;
-                        res.end(pretty.print(result));
-                    } else {
-                        res.statusCode = 200;
-                        res.end(pretty.print([]));
-                    }
-                });
+                query.tracks(res, params);
             } else {
-                res.statusCode = 401;
-                res.send('401 Unauthorized');
+                serverResponse.unauthorized(res);
             }
         });
     },
@@ -52,31 +41,21 @@ module.exports = {
         var apikey = req.param('apikey');
         Token.findOne({'value': apikey}, function (err, token) {
             if (err) {
-                res.statusCode = 500;
-                res.end(pretty.print(excep.msg(500, 'Server Error', err)));
+                serverResponse.error(err);
                 return;
             }
             if (token) {
-                Track.find({'_id': req.params.id}, function (err, result) {
-                    res.setHeader("Content-Type", "application/json");
+                var searchObject = {};
+                searchObject._id = req.params.id;
 
-                    if (err) {
-                        console.log(err);
-                        res.statusCode = 500;
-                        res.end(pretty.print(excep.msg(500, 'Server Error', err)));
-                        return;
-                    }
-                    if (result.length) {
-                        res.statusCode = 200;
-                        res.end(pretty.print(result));
-                    } else {
-                        res.statusCode = 200;
-                        res.end(pretty.print([]));
-                    }
-                });
+                var params = {
+                    limit:req.param('limit'),
+                    offset:req.param('offset'),
+                    searchTerm:searchObject
+                };
+                query.tracks(res, params);
             } else {
-                res.statusCode = 401;
-                res.send('401 Unauthorized');
+                serverResponse.unauthorized(res);
             }
         });
     },
@@ -85,43 +64,22 @@ module.exports = {
         var apikey = req.param('apikey');
         Token.findOne({'value': apikey}, function (err, token) {
             if (err) {
-                res.statusCode = 500;
-                res.end(pretty.print(excep.msg(500, 'Server Error', err)));
+                serverResponse.error(err);
                 return;
             }
             if (token) {
-                Artist.findOne({'_id': req.params.id}, function (err, artist) {
-                    res.setHeader("Content-Type", "application/json");
+                var searchObject = {};
+                searchObject._id = req.params.id;
 
-                    if (err) {
-                        console.log(err);
-                        res.statusCode = 500;
-                        res.end(pretty.print(excep.msg(500, 'Server Error', err)));
-                    }
-                    if (artist) {
-                        Track.find({'artists.artistName':  artist.artistName}, function (err, result){
-                            if (err) {
-                                console.log(err);
-                                res.statusCode = 500;
-                                res.end(pretty.print(excep.msg(500, 'Server Error', err)));
-                                return;
-                            }
-                            if (result.length) {
-                                res.statusCode = 200;
-                                res.end(pretty.print(result));
-                            } else {
-                                res.statusCode = 200;
-                                res.end(pretty.print([]));
-                            }
-                        });
-                    } else {
-                        res.statusCode = 200;
-                        res.end(pretty.print([]));
-                    }
-                });
+                var params = {
+                    limit:req.param('limit'),
+                    offset:req.param('offset'),
+                    searchTerm:searchObject
+                };
+                query.tracksByArtist(res, params);
+
             } else {
-                res.statusCode = 401;
-                res.send('401 Unauthorized');
+                serverResponse.unauthorized(res);
             }
         });
     }
--- queriers/tracksQuerier.js
+++ queriers/tracksQuerier.js
@@ -1,7 +1,88 @@
+require('../models/artist')();
+
 var mongoose = require('mongoose');
 var pretty = require('../utils/pretty');
 var config = require('config');
+var serverResponse = require('../utils/serverResponse.js');
+var pagination = require('../utils/pagination.js');
 
 //max 4 connections in pool
 var conn = mongoose.createConnection(config.get('db_uri'),{ server: { poolSize: 4 }});
-//Album and Artist models are scoped to above specific connection object
+//Artist model is scoped to above specific connection object
+var Artist = conn.model('Artist');
+var Track = conn.model('Track');
+
+module.exports = {
+
+    tracks: function queryTracks(res,params){
+        var range = pagination.range(params);
+        var searchTerm = params.searchTerm;
+        console.log("searchQuery: %s", pretty.print(searchTerm));
+
+        Track.find(searchTerm).paginate(range.offset, range.limit, function(err, docs, total) {
+            params.total = total;
+            var paging = pagination.paging(res,params);
+
+            if(paging.offset < 1 ) {
+                serverResponse.invalid_range(res);
+                return;
+            }
+
+            var result = {
+                totalEvents: total,
+                totalPages: paging.totalPages,
+                currentPage: paging.offset,
+                pagination: {
+                    prev: paging.prev,
+                    next: paging.next
+                },
+                result:docs
+            };
+
+            serverResponse.result(res,result);
+        });
+
+    },
+
+    tracksByArtist:function(res,params){
+        var range = pagination.range(params);
+        var searchTerm = params.searchTerm;
+        var result = {};
+
+        console.log("searchQuery: %s", pretty.print(searchTerm));
+
+        Artist.findOne(searchTerm, function (err, artist) {
+            if (err) {
+                serverResponse.error(res, err);
+                return
+            }
+            if (artist) {
+                Track.find({'artists.artistName':  artist.artistName}).paginate(range.offset, range.limit, function(err, docs, total) {
+                    params.total = total;
+                    var paging = pagination.paging(res,params);
+
+                    if(paging.offset < 1 ) {
+                        serverResponse.invalid_range(res);
+                        return;
+                    }
+
+                    result = {
+                        totalEvents: total,
+                        totalPages: paging.totalPages,
+                        currentPage: paging.offset,
+                        pagination: {
+                            prev: paging.prev,
+                            next: paging.next
+                        },
+                        result:docs
+                    };
+
+                    serverResponse.result(res,result);
+                });
+            } else {
+                serverResponse.result(res, result);
+            }
+        });
+    }
+
+};
