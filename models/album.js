f88fb9f1c32abf42e71903a034598389c2d25373
srmds <stevenramdas@gmail.com>
2015-06-27 00:02:34 +0200

    cleanup

--- models/album.js
+++ models/album.js
@@ -1,7 +1,5 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-//require('../models/artist')();
-//var Artist = mongoose.model('Artist');
 
 var albumSchema = new Schema({
     genres: [{genreName: String}],
