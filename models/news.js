b2ec9642da8f0d9034eb8885d67c1d92e54e7840
srmds <stevenramdas@gmail.com>
2015-06-30 00:16:41 +0200

    Add News model #26

---
+++ models/news.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var newsSchema = new Schema({
+    date: String,
+    title: String,
+    message: String
+});
+
+module.exports = mongoose.model('Event', newsSchema);
