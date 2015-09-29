5797c12d27997174b7bc7461697836f5952790e1
srmds <stevenramdas@gmail.com>
2015-09-29 18:04:15 +0200

    remove old references

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
-
 var express = require('express');
 var router = express.Router();
 var path = require("path");
-var passwordless = require('passwordless');
 var bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
@@ -12,10 +10,6 @@ router.get('/', function(req, res) {
     res.sendFile('api-overview.html', { root: path.join(__dirname, '../public') });
 });
 
-router.get('/token-request', function(req, res) {
-    res.sendFile('token-request.html', { root: path.join(__dirname, '../public') });
-});
-
 router.get('/send', function(req, res) {
     res.sendFile('send.html', { root: path.join(__dirname, '../public') });
 });
@@ -23,16 +17,5 @@ router.get('/send', function(req, res) {
 router.get('/404', function(req, res) {
     res.sendFile('404.html', { root: path.join(__dirname, '../public') });
 });
-
-router.post('/sendtoken',
-    passwordless.requestToken(
-        function(user, delivery, callback, req) {
-            callback(null, user);
-        }),
-    function(req, res) {
-        res.writeHead(302, {'Location': '/send'});
-            res.end();
-    }
-);
 
 module.exports = router;
